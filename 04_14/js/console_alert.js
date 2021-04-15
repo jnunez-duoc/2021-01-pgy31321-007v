67def602d99a63cd3b8f4b29d329c0a56d103fc2
Jose Nuñez <j.nunez@profesor.duoc.cl>
2021-04-14 21:38:04 -0400

    Clase 14/04

---
+++ 04_14/js/console_alert.js
@@ -0,0 +1,28 @@
+console.log('Hola Mundo!')
+console.info('Informacion de la consola')
+console.error('Error en la consola')
+
+// Ejemplo de Var
+var variable_1;
+variable_1 = 'Hola desde variable_1'
+console.log(variable_1)
+var variable_1 = 'Hola desde variable_1'
+console.log(variable_1)
+
+
+// Ejemplo de let
+let variable_2 = 'Hola desde variable_2'
+console.log(variable_2)
+
+if (true) {
+    let variable_2 = 'Hola desde variable_2 reinicializado.'
+    console.log(variable_2)
+}
+variable_2 = 'Chao desde variable_2'
+console.log(variable_2)
+
+// Ejemplo de const
+const VARIABLE_3 = 'Hola desde variable_3'
+console.log(VARIABLE_3)
+
+alert('Hola, soy una alerta!')
---
+++ 04_14/js/script.js
@@ -0,0 +1,3 @@
+let contenido_pagina = document.getElementById('contenido_pagina')
+console.log(contenido_pagina)
+contenido_pagina.innerHTML = 'Hola! Desde Javascript :D'
